a8425bffc10eb3bfd4069c26666caf74e2c8dc4c
majorjupiter <mjfuencias@gmail.com>
2021-11-13 23:51:56 +0800

    Activity 3 - Added an if-else statement.

--- Activity 3 - Reactivity/app.js
+++ Activity 3 - Reactivity/app.js
@@ -3,10 +3,13 @@ const app = Vue.createApp ({
     data()
     {
         return {
-            value: 0
+            value: 0,
+            timer:5000,
         };
 
     },
+
+    
 
     methods: 
     {
@@ -14,10 +17,28 @@ const app = Vue.createApp ({
         add(int)
         {
             this.value = this.value+int
-        }
+        },
         
+        result() 
+        {
+            if (this.value > 37) 
+          {
+              return 'Too Much!'
+          }
+          else if (this.value === 37 || this.value === 0)
+          {
+              return this.value
+          }
+          else 
+          {
+              return 'Not There Yet!'
+          }
+
+        },
+
     },
-    
+
+
 });
 
 app.mount('#content');
